bd6c880eb65a6b5c31e7839fc257c9f8296234f4
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-22 15:33:02 -0300

    Makes course class "Método abstrato"

---
+++ src/aulas/09.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Conta(metaclass=ABCMeta):
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
+
+    def __str__(self):
+        return "[>>Código {} e Saldo {}<<]".format(self._codigo, self._saldo)
+
+
+class ContaCorrente(Conta):
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+
+class ContaPoupanca(Conta):
+    def passa_o_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
+
+
+class ContaInvestimento(Conta):
+    pass
+
+ContaPoupanca(15)
+ContaInvestimento(10)
