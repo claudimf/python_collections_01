0d2a58248c8d48a1447b0ce5779c0c04785973a8
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-22 16:02:16 -0300

    Makes course class "Ordenação básica"

---
+++ src/aulas/12.py
@@ -0,0 +1,15 @@
+idades = [15, 87, 32, 65, 56, 32, 49, 37]
+
+print(sorted(idades))
+
+print(list(reversed(idades)))
+
+print(sorted(idades, reverse=True))
+
+print(list(reversed(sorted(idades))))
+
+print(idades)
+
+idades.sort()
+
+print(idades)
